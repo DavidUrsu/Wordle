f73304ada9619f8342ae97517f809bd5b1132fc6
Sparkle12 <50665469+Sparkle12@users.noreply.github.com>
2022-11-27 17:04:21 +0200

    Delete listener.py

--- listener.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing.connection import Listener
-
-address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
-listener = Listener(address, authkey=b'secret password')
-conn = listener.accept()
-print ("connection accepted from" + str(listener.last_accepted))
-msg = conn.recv()
-print(msg)
-listener.close()
